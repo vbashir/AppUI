20dd2c6d032c2615f97646f3fbb6b7ec7877142c
Башир Валиев <bashir.valiev@lynkage.ru>
2021-08-30 10:23:37 +0300

    Initial Commit

---
+++ AppUI/AppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppUIApp.swift
+//  AppUI
+//
+//  Created by Башир Валиев on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppUI
+//
+//  Created by Башир Валиев on 30.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
